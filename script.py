09e4ddf62de7fc9e431d2f4cc2dbcefd645540d9
asd-35 <saridoganarda35@gmail.com>
2021-07-21 23:39:28 +0300

    piano bot done fixin up the menu

--- script.py
+++ script.py
@@ -2,44 +2,33 @@ import pyautogui
 import PIL.ImageGrab
 
 
-black = (8,8,8)
+black = (8,8,8) #in my gaem rgb values of the black was like this
 
-print("Mozart plays")
+print(
+  "__  __  ____ ______         _____ _______\n" +
+ "|  \/  |/ __ \___  /   /\   |  __ \__   __|\n"+
+ "| \  / | |  | | / /   /  \  | |__) | | |\n"+   
+ "| |\/| | |  | |/ /   / /\ \ |  _  /  | |\n"+   
+ "| |  | | |__| / /__ / ____ \| | \ \  | |\n"+  
+ "|_|  |_|\____/_____/_/    \_\_|  \_\ |_|\n"   
+    )
 
 while True:
     
     
-    first = [PIL.ImageGrab.grab().load()[150,1000] == black,
-    PIL.ImageGrab.grab().load()[150,975] == black,
-    PIL.ImageGrab.grab().load()[150,985] == black
-    ]
+  
     
-    second = [PIL.ImageGrab.grab().load()[589, 975] == black,
-    PIL.ImageGrab.grab().load()[589, 1000] == black,
-    PIL.ImageGrab.grab().load()[589, 985] == black
-    ]
-    
-    third = [PIL.ImageGrab.grab().load()[1028, 1000] == black,
-    PIL.ImageGrab.grab().load()[1028, 975] == black,
-    PIL.ImageGrab.grab().load()[1028, 985] == black
-    ]
-    
-    fourth = [PIL.ImageGrab.grab().load()[1600, 1000] == black,
-    PIL.ImageGrab.grab().load()[1600, 975] == black,
-    PIL.ImageGrab.grab().load()[1600, 985] == black
-    ]
-    
-    if first.count(True) == 3:
-        pyautogui.click(150,975)
-        print(first,"1nd")
-    elif second.count(True) == 3:
-        pyautogui.click(589, 975)
-        print(second,"2nd")
-    elif third.count(True) == 3:
-        pyautogui.click(1028, 975)
-        print(third,"3rd")
-    elif fourth.count(True) == 3:
-        pyautogui.click(1600, 975)
-        print(fourth,"4th")
+    if PIL.ImageGrab.grab().load()[150, 700] == black:
+        pyautogui.click(150,625)
+        print("1nd")
+    elif PIL.ImageGrab.grab().load()[589, 700] == black:
+        pyautogui.click(589, 725)
+        print("2nd")
+    elif PIL.ImageGrab.grab().load()[1028, 700] == black:
+        pyautogui.click(1028, 725)
+        print("3rd")
+    elif PIL.ImageGrab.grab().load()[1600, 700] == black:
+        pyautogui.click(1600, 725)
+        print("4th")
    
 
